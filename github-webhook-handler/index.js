414789195a85bb382e43e45ca1c5f6231084e12c
tung.pt <phamthanhtung.vienthonga.com>
2016-10-27 11:14:44 +0700

    feat(refactor)

--- github-webhook-handler/index.js
+++ github-webhook-handler/index.js
@@ -37,16 +37,19 @@ function create(options) {
 
 
   function handler(req, res, callback) {
-    if (req.headers['x-github-event'] || req.headers['x-gitlab-event']) {
-      handlerGithubOrGitlap(req, res, callback);
+    if (req.headers['x-github-event']) {
+      handlerGithub(req, res, callback);
     }
     if (req.headers['x-gogs-event']) {
       handlerGogs(req, res, callback);
     }
+     if (req.headers['x-gitlab-event']) {
+      handlerGitLap(req, res, callback);
+    }
 
   }
 
-  function handlerGithubOrGitlap(req, res, callback) {
+  function handlerGithub(req, res, callback) {
     console.log('on github handler');
     if (req.url.split('?').shift() !== options.path)
       return callback();
@@ -66,19 +69,19 @@ function create(options) {
     }
 
     var sig = req.headers['x-hub-signature'];
-    var token = req.headers['x-gitlab-token'];
+  
 
-    var event = req.headers['x-github-event'] || req.headers['x-gitlab-event'];
+    var event = req.headers['x-github-event'] ;
     var id = req.headers['x-github-delivery'];
 
-    if (!sig)
+    if (!sig && (typeof sig != 'undefined' ))
      return hasError('No X-Hub-Signature or X-Gitlab-Token found on request');
 
     if (!event)
       return hasError('No X-Github-Event or X-Gitlab-Event found on request');
 
-    //if (!id)
-    // return hasError('No X-Github-Delivery or X-Gitlab-Delivery found on request')
+    if (!id)
+    return hasError('No X-Github-Delivery or X-Gitlab-Delivery found on request')
 
     if (events && events.indexOf(event) == -1)
       return hasError('X-Github-Event is not acceptable');
@@ -89,21 +92,12 @@ function create(options) {
       if (err) {
         return hasError(err.message);
       }
-      var obj
+      var obj;
       var computedSig = new Buffer(signBlob(options.secret, data))
 
       if (!bufferEq(new Buffer(sig), computedSig))
         return hasError('X-Hub-Signature does not match blob signature');
-      else
-      // check gitlap secret key
-      if (token) {
-        if (options.secret !== token)
-          return hasError('X-Gitlab-Token does not match');
-      } else
-      // check gogs webhook secret key 
-      {
-        return hasError('missing options.secret');
-      }
+  
 
 
       try {
@@ -133,10 +127,81 @@ function create(options) {
 
       handler.emit(event, emitData);
       handler.emit('*', emitData);
-    }))
+    }));
+  }
+
+  function handlerGitLap(req, res, callback){
+    console.log('on gitlap handler');
+    if (req.url.split('?').shift() !== options.path)
+      return callback();
+
+    function hasError(msg) {
+      res.writeHead(400, {
+        'content-type': 'application/json'
+      });
+      res.end(JSON.stringify({
+        error: msg
+      }));
+
+      var err = new Error(msg);
+
+      handler.emit('error', err, req);
+      callback(err);
+    }
+
+   
+    var token = req.headers['x-gitlab-token'];
+
+    var event = req.headers['x-gitlab-event'];
+
+
+    if (!event)
+      return hasError('No X-Github-Event or X-Gitlab-Event found on request');
+
+    
+
+  
+  
+    req.pipe(bl(function (err, data) {
+      var obj;
+      if (token) {
+        if (options.secret !== token)
+          return hasError('X-Gitlab-Token does not match');
+      } 
+      else
+   
+        return hasError('missing options.secret');
+      
+      try {
+        obj = JSON.parse(data)
+      } catch (e) {
+        return hasError(e)
+      }
+
+      res.writeHead(200, {
+        'content-type': 'application/json'
+      })
+      res.end('{"ok":true}');
+
+      if (req.headers['x-gitlab-event']) {
+        event = obj['event_name'];
+      }
+
+      var emitData = {
+        event: event,
+        payload: obj,
+        protocol: req.protocol,
+        host: req.headers['host'],
+        url: req.url
+      }
+
+      handler.emit(event, emitData);
+      handler.emit('*', emitData);
+    }));
   }
 
   function handlerGogs(req, res, callback) {
+    console.log('on gogs handler');
     if (req.url.split('?').shift() !== options.path)
       return callback();
 
